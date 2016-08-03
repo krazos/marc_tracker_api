40c2e4ef712a8ab43f17968f38f24363bc27ba33
krazos <krazos@gmail.com>
2016-08-03 09:00:41 -0400

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,27 +1,14 @@
-<?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
+<?php
+require 'scraperwiki.php';
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
+require 'scraperwiki/simple_html_dom.php';           
+$html_content = scraperwiki::scrape("http://www.marctracker.com/PublicView/location.jsp");
+$html = str_get_html($html_content);
 
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
+ foreach ($html->find("area") as $el) { 
+    $a =  $el->title;
+    if (strpos($a,'Train') !== false) {
+        echo $a . "\n";
+    }         
+}
 ?>
