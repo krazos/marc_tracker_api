0755359b782dfe1f7570865057e368fe7518dac6
krazos <krazos@gmail.com>
2016-08-03 09:48:19 -0400

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -4,12 +4,16 @@ require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';           
 $html_content = scraperwiki::scrape("http://www.marctracker.com/PublicView/location.jsp");
 $html = str_get_html($html_content);
+$train_status = array();
 
  foreach ($html->find("area") as $el) { 
     $a =  $el->title;
     if (strpos($a,'Train') !== false) {
         echo $a . "\n";
-        scraperwiki::save_sqlite(array("train_status"), array("train_status" => $a));
+        $train_status[] = $a;
     }         
 }
+
+scraperwiki::save_sqlite(array("train_status"), array("train_status" => $train_status));
+
 ?>
